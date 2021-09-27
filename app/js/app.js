bbee2957af2ef0f25657a3abb5a4ffce91524ec2
Vitaliy <vitaliynosov2014@gmail.com>
2021-09-27 14:44:31 +0300

    commit

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
-	console.log('dc');
+	
+	// console greetings
+	let jadoo = '"Jadoo" html test'
+	console.log ( '%c%s', 'color: #F1A501; font: 0.8rem/1 Tahoma;', jadoo );
+	let autor = 'Developer: Vitaliy Nosov';
+	console.log ( '%c%s', 'color: #DF6951; font: 1.2rem/1 Tahoma;', autor );
+
+	// burger menu
 
 	$('#burger-icon').click(function () {
 		$('.menu-block').slideToggle(500);
